01560158d5526cc1b69cb2dbb27f5aef2f86331b
Dale O'Brien <dale@mocomedia.net>
2012-09-20 10:30:21 +1000

    added setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+__version__ = '1.0'
+
+description = '''App that creates xls reports, or tables, from simple lists'''
+
+setup(
+    name='workbook',
+    packages=find_packages(),
+    version=__version__,
+    description=description,
+    author='Dale O\'Brien',
+    author_email='dale@do.id.au',
+    url='https://github.com/daleobrien/workbook',
+    install_requires=(
+        'xlutils',
+        'xlwt'
+        ),
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+        )
+ )
